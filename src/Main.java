9e5a0ec6ee8830781d557cb7ed7b126445ad4535
divya <divyabehere@gmail.com>
2017-08-10 13:14:02 -0700

    add main class file

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=10,b=5,c;
+		c=a+b;
+		System.out.println("Sum is " +c);
+		
+		
+	}
+
+}
